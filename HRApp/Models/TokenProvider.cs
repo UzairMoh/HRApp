7e3c2cf50018ef86e5bf2ef1c97268a822eee3f3
Uzair <c3058311@hallam.shu.ac.uk>
2024-10-28 15:22:29 +0000

    Added Auth0 to the application

---
+++ HRApp/Models/TokenProvider.cs
@@ -0,0 +1,9 @@
+namespace HRApp.Models;
+
+public class TokenProvider
+{
+    public string? IdToken { get; set; }
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
+
