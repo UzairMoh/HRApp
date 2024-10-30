be6178679ab74a181a074f4e3cce3120776d7ba6
Uzair Mohammed <c3058311@hallam.shu.ac.uk>
2024-10-30 11:13:21 +0000

    Updated model

--- HRApp/Models/Employee.cs
+++ HRApp/Models/Employee.cs
@@ -1,13 +1,51 @@
-﻿namespace HRApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRApp.Models
 {
+    /// <summary>
+    /// Represents an employee in the HR application.
+    /// </summary>
     public class Employee
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the employee.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of the employee.
+        /// </summary>
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string? FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of the employee.
+        /// </summary>
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string? LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address of the employee.
+        /// </summary>
+        [StringLength(100, ErrorMessage = "Email cannot be longer than 100 characters.")]
         public string? Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the gender of the employee.
+        /// </summary>
+        [StringLength(10, ErrorMessage = "Gender cannot be longer than 10 characters.")]
         public string? Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department of the employee.
+        /// </summary>
+        [StringLength(50, ErrorMessage = "Department cannot be longer than 50 characters.")]
         public string? Department { get; set; }
+
+        /// <summary>
+        /// Gets or sets the salary of the employee.
+        /// </summary>
+        [StringLength(20, ErrorMessage = "Salary cannot be longer than 20 characters.")]
         public string? Salary { get; set; }
     }
 }
--- HRApp/Pages/Home.razor.cs
+++ HRApp/Pages/Home.razor.cs
@@ -1,70 +1,59 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using HRApp.Services;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using HRApp.Models;
 
-namespace HRApp.Pages
+namespace HRApp.Pages;
+
+public partial class Home : ComponentBase
 {
-    public partial class Home : ComponentBase
+    [Inject] public IEmployeeService EmployeeService { get; set; } = null!;
+
+    [Inject] public IJSRuntime JsRuntime { get; set; } = null!;
+
+    [Inject] public TokenProvider TokenProvider { get; set; } = null!;
+
+    private int TotalEmployees { get; set; }
+    private int TotalMaleEmployees { get; set; }
+    private int TotalFemaleEmployees { get; set; }
+    private Dictionary<string, int> EmployeesByDepartment { get; set; } = new();
+
+    private bool _isChartLoaded;
+
+    protected override async Task OnInitializedAsync()
     {
-        [Inject]
-        public IEmployeeService EmployeeService { get; set; } = null!;
+        var employees = await EmployeeService.GetEmployeesAsync();
 
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; } = null!;
+        TotalEmployees = employees.Count;
+        TotalMaleEmployees = employees.Count(e => e.Gender == "Male");
+        TotalFemaleEmployees = employees.Count(e => e.Gender == "Female");
+        EmployeesByDepartment = employees
+            .GroupBy(e => e.Department)
+            .ToDictionary(g => g.Key!, g => g.Count());
         
-        [Inject]
-        public TokenProvider TokenProvider { get; set; } = null!;
+        await base.OnInitializedAsync();
+    }
 
-        private int TotalEmployees { get; set; }
-        private int TotalMaleEmployees { get; set; }
-        private int TotalFemaleEmployees { get; set; }
-        private Dictionary<string, int> EmployeesByDepartment { get; set; } = new Dictionary<string, int>();
-
-        private bool _isChartLoaded;
-        
-        private string IdToken = "";
-
-        protected override async Task OnInitializedAsync()
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && !_isChartLoaded)
         {
-            var employees = await EmployeeService.GetEmployeesAsync();
-
-            TotalEmployees = employees.Count;
-            TotalMaleEmployees = employees.Count(e => e.Gender == "Male");
-            TotalFemaleEmployees = employees.Count(e => e.Gender == "Female");
-            EmployeesByDepartment = employees
-                .GroupBy(e => e.Department)
-                .ToDictionary(g => g.Key, g => g.Count());
-            
-            IdToken = TokenProvider.IdToken;
-
-            await base.OnInitializedAsync();
+            await LoadModule();
+            await LoadDepartmentChart();
+            _isChartLoaded = true;
         }
+    }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender && !_isChartLoaded)
-            {
-                await LoadModule();
-                await LoadDepartmentChart();
-                _isChartLoaded = true;
-            }
-        }
+    private async Task LoadModule()
+    {
+        await JsRuntime.InvokeVoidAsync("import", "./Pages/Home.razor.js");
+    }
 
-        private async Task LoadModule()
-        {
-            await JSRuntime.InvokeVoidAsync("import", "./Pages/Home.razor.js");
-        }
+    private async Task LoadDepartmentChart()
+    {
+        var departmentLabels = EmployeesByDepartment.Keys.ToArray();
+        var departmentCounts = EmployeesByDepartment.Values.ToArray();
 
-        private async Task LoadDepartmentChart()
-        {
-            var departmentLabels = EmployeesByDepartment.Keys.ToArray();
-            var departmentCounts = EmployeesByDepartment.Values.ToArray();
-
-            await JSRuntime.InvokeVoidAsync("renderDepartmentChart", departmentLabels, departmentCounts);
-        }
+        await JsRuntime.InvokeVoidAsync("renderDepartmentChart", departmentLabels, departmentCounts);
     }
 }
