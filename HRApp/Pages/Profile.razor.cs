d686589649589574879c98d1e0ded376fe677295
Uzair Mohammed <c3058311@hallam.shu.ac.uk>
2024-10-30 10:50:03 +0000

    Added department and salary

--- HRApp/Pages/Profile.razor.cs
+++ HRApp/Pages/Profile.razor.cs
@@ -1,33 +1,41 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using HRApp.Services;
+using HRApp.Models;
 
 namespace HRApp.Pages
 {
     public partial class Profile
     {
+        [Inject]
+        private IEmployeeService? EmployeeService { get; set; }
+
         [CascadingParameter]
-        public Task<AuthenticationState> AuthenticationStateTask { get; set; }
-        private string Username = "";
-        private string EmailAddress = "";
-        private string Picture = "";
-        private string IdToken = "";
+        public Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+        private Employee? UserProfile { get; set; }
+        
+        private string _picture = "";
 
         protected override async Task OnInitializedAsync()
         {
             var state = await AuthenticationStateTask;
-
-            Username = state.User.Identity.Name ?? string.Empty;
-            EmailAddress = state.User.Claims
+            var email = state.User.Claims
                 .Where(c => c.Type.Equals(System.Security.Claims.ClaimTypes.Email))
                 .Select(c => c.Value)
-                .FirstOrDefault() ?? string.Empty;
+                .FirstOrDefault();
 
-            Picture = state.User.Claims
+            if (!string.IsNullOrEmpty(email))
+            {
+                UserProfile = (await EmployeeService.GetEmployeesAsync())
+                    .FirstOrDefault(e => e.Email == email);
+            }
+            
+            _picture = state.User.Claims
                 .Where(c => c.Type.Equals("picture"))
                 .Select(c => c.Value)
                 .FirstOrDefault() ?? string.Empty;
 
-            IdToken = tokenProvider.IdToken;
 
             await base.OnInitializedAsync();
         }
