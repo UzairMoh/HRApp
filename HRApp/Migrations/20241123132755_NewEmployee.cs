ed208a4d84ee4180e60d1900e60d926b7dd4cf18
Uzair <c3058311@hallam.shu.ac.uk>
2024-11-23 13:30:31 +0000

    Updated migrations and hid employees page from non admins

---
+++ HRApp/Migrations/20241123132755_NewEmployee.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FirstName", "Gender", "LastName" },
+                values: new object[] { "uzairmohammedpc@gmail.com", "Uzair", "Male", "Mohammed" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FirstName", "Gender", "LastName" },
+                values: new object[] { "schen@company.com", "Sarah", "Female", "Chen" });
+        }
+    }
+}
